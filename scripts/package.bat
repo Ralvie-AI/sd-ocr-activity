6c91a77098ef2fb8d572161ee5e9e27b3f20aa4c
hello0zai <hello0zaizai@gmail.com>
2026-08-31 13:21:12 +0700

    delete unused files

--- scripts/package.bat
+++
@@ -1,5 +0,0 @@
-set PATH=%PATH%;D:\programs\anaconda3113_2;D:\programs\anaconda3113_2\Library\mingw-w64\bin;D:\programs\anaconda3113_2\Library\usr\bin;D:\programs\anaconda3113_2\Library\bin;D:\programs\anaconda3113_2\Scripts
-
-echo PATH=%PATH%
-
-pyinstaller sd-ocr-activity.spec --clean --noconfirm
